13bf212de3912cc01d0f5b2d058a31fe452b189a
Bansheebomb <dugiev123@gmail.com>
2022-02-09 21:56:40 +0100

    first database integration and person
    might be buggy lol

---
+++ src/model/Person.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+class Person extends Sequelize.Model {
+  static createModel(sequelize) {
+    /**
+   * Defines the Person entity from the database
+   * @param {Sequelize} sequelize The sequelize object.
+   * @return {Model} A sequelize model describing the Person entity.
+   */
+    Person.init(
+        {
+          username: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          password: {
+              type: Sequelize.STRING,
+              allowNull: false,
+          }
+        },
+        {sequelize, modelName: 'person', paranoid: true}
+    );
+    return Person;
+  }
+}
