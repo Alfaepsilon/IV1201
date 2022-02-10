09e96a6759857e3c4422d43266f66b1ee7efcca4
Bansheebomb <dugiev123@gmail.com>
2022-02-10 17:29:31 +0100

    Update Controller.js

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,25 +1,25 @@
 'use strict';
 
-import Integration from "../integration/Integration.js";
+const recruitDAO = require('../integration/recruitDAO');
 
-  /**
-   * Login a user. This is not a real login since no password is required. The
-   * only check that is performed is that the username exists in the database.
-   *
-   * @param {string} username: The username of the user logging in.
-   * @param {string} password: The password of the user logging in.
-   * @return {User} The logged in user if login succeeded, or null if login
-   *                failed.
-   * @throws Throws an exception if unable to attempt to login the specified
-   *         user.
-   */
-  function login(username, password) {
-      var check_login = await this.Integration.login(username, password);
-      if (check_login === false) {
-        return false;
-      }
-      else if(check_login === true){
-        return true;
-      }
+class Controller
+{
+
+  constructor()
+  {
+    this.recruitDAO = new recruitDAO();
   }
+
+  static async createController()
+  {
+    const controller = new Controller();
+    await controller.recruitDAO.makeTables();
+    return controller;
+  }
+
+  async login(username, password) {
+      var check_login = await this.recruitDAO.login(username, password);
+      return check_login;
+  }
+}
 module.exports = Controller;
