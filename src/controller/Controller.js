0cced8f4b0b4d2c938be29d639a0909895b11807
Bansheebomb <dugiev123@gmail.com>
2022-03-14 03:05:21 +0100

    fixed register in controller

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -54,10 +54,19 @@ class Controller {
      * @param {String}  password The password of our user.
      * @param {String}  role_id The role identification of our user.
      */
-   async register(name, surname, email, pnr, username, password, role_id)
+   async register(user)
    {
      try{return this.transactions.transaction(async(x) => {
-     await this.recruitDAO.register(name, surname, email, pnr, username, password, role_id);
+      var isCreated = await this.recruitDAO.register(user);
+      console.log(isCreated)
+      console.log(Object.keys(isCreated).length)
+      if (Object.keys(isCreated).length >= 1) {
+       console.log(true);
+       return true
+     } else {
+       console.log(false)
+       return false;
+     }
      });}
      catch(error){console.log(error)}
    }
